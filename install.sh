ec1f5d3dbdf3350a9ae6f7c79981b7e2626b354b
James Edmonston <james@ethercreative.co.uk>
2020-09-21 13:38:27 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -5,59 +5,63 @@ brew install\
 
 yarn global add expo;
 
-expo init --name Starter --template bare-typescript;
+expo init --name Starter --template expo-template-bare-typescript;
 
 mv Starter/* .;
 
-expo install @apollo/client\
-             @expo/react-native-action-sheet\
-             @react-native-community/async-storage\
-             @react-native-community/datetimepicker\
-             @react-native-community/masked-view\
-             @react-navigation/bottoms-tabs\
-             @react-navigation/core\
-             @react-navigation/native\
-             @react-navigation/native-stack\
-             @react-navication/stack\
-             apollo-cache-persist\
-             dayjs\
-             expo\
-             expo-constants\
-             expo-linear-gradient\
-             expo-linking\
-             expo-splash-screen\
-             graphql\
-             graphql-tag\
-             react-native-gesture-handler\
-             react-native-global-props\
-             react-native-iphone-x-helper\
-             react-native-reanimated\
-             react-native-render-html\
-             react-native-safe-area-context\
-             react-native-screens\
-             react-native-tailwindcss\
-             react-native-webview\
-             react-navigation\
-             react-navigation-stack\
-             sentry-expo;
+expo install\
+  @apollo/client\
+  @expo/react-native-action-sheet\
+  @react-native-community/async-storage\
+  @react-native-community/datetimepicker\
+  @react-native-community/masked-view\
+  @react-navigation/bottoms-tabs\
+  @react-navigation/core\
+  @react-navigation/native\
+  @react-navigation/native-stack\
+  @react-navication/stack\
+  apollo-cache-persist\
+  dayjs\
+  expo\
+  expo-constants\
+  expo-linear-gradient\
+  expo-linking\
+  expo-splash-screen\
+  graphql\
+  graphql-tag\
+  react-native-gesture-handler\
+  react-native-global-props\
+  react-native-iphone-x-helper\
+  react-native-reanimated\
+  react-native-render-html\
+  react-native-safe-area-context\
+  react-native-screens\
+  react-native-tailwindcss\
+  react-native-webview\
+  react-navigation\
+  react-navigation-stack\
+  sentry-expo;
 
-yarn add --dev @graphql-codegen/cli\
-               @graphql-codegen/typescript\
-               @graphql-codegen/typescript-operations\
-               @types/react\
-               @types/react-native\
-               @typescript-eslint/eslint-plugin\
-               @typescript-eslint/parser\
-               babel-preset-expo\
-               eslint\
-               eslint-config-universe\
-               husky\
-               imagemin\
-               imagemin-jpegoptim\
-               imagemin-optipng\
-               imagemin-svgo\
-               lint-staged\
-               prettier\
-               typescript;
+yarn add --dev\
+  @graphql-codegen/cli\
+  @graphql-codegen/typescript\
+  @graphql-codegen/typescript-operations\
+  @types/react\
+  @types/react-native\
+  @typescript-eslint/eslint-plugin\
+  @typescript-eslint/parser\
+  babel-preset-expo\
+  eslint\
+  eslint-config-universe\
+  husky\
+  imagemin\
+  imagemin-jpegoptim\
+  imagemin-optipng\
+  imagemin-svgo\
+  lint-staged\
+  prettier\
+  typescript;
 
-rm install.sh;
+cd ios && pod install;
+
+cd ../ && rm install.sh;
