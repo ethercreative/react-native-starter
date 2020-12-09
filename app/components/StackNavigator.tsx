e720f10687e8986e221b9c260811acc5806c4519
James Edmonston <james@ethercreative.co.uk>
2020-12-09 17:50:04 +0000

    Fix React import

--- app/components/StackNavigator.tsx
+++ app/components/StackNavigator.tsx
@@ -1,4 +1,4 @@
-import * React from 'react';
+import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from '@react-navigation/native';
 
