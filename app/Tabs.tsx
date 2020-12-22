a9e8e65118cd2a5efd47eaf484ca99a50a03c20c
James Edmonston <james@ethercreative.co.uk>
2020-12-22 12:32:29 +0000

    Update Tabs.tsx

--- app/Tabs.tsx
+++ app/Tabs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useTheme } from '@react-navigation/native';
