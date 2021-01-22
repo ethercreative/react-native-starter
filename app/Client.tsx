4818c84c547c5b212275d3e6e1b4b7e9e51d058d
James Edmonston <james@ethercreative.co.uk>
2021-01-22 17:26:04 +0000

    Update Client.tsx

--- app/Client.tsx
+++ app/Client.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {
   ApolloClient,
